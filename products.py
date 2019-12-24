7590fcf2409b84e574bc8244f83548bc57019267
Emily Hu <Smile0Hu55873@gmail.com>
2019-12-24 22:37:45 +0800

    add for loop

--- products.py
+++ products.py
@@ -4,8 +4,11 @@ while True:
 	 if name == 'q': #逃出迴圈
 	 		break
 	 price = input ('請輸入價格:')
-	 p = []
+	 p = [] # p = [nema, price]
 	 p.append(name)
 	 p.append(price)
-	 products.append(p)
+	 products.append(p)# append([name, price])
 print(products)
+
+for p in products:
+	print(p[0], '的價格是', p[1])
